1571779ddfef1b539edc3d0986422e272b29689b
Somon1998 <karimzoda.somon@gmail.com>
2025-04-13 02:22:54 +0500

    finish project

---
+++ app.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => observer.observe(box));
